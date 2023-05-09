cedf91ab84186cce6e8ba25c518c242ba9cea965
fuhewat <fuhechan@yahoo.co.jp>
2023-05-09 21:25:14 +0900

    サブ

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -35,6 +35,7 @@ RSpec.describe Message, type: :model do
         @message.user = nil
         @message.valid?
         expect(@message.errors.full_messages).to include('User must exist')
+        
       end
     end
   end
